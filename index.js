266ae439d913cd4fd8624d7e0b804af9fb500efc
najuml-bd <najmul0612@gmail.com>
2022-07-02 00:27:53 +0600

    API added for edit task

--- index.js
+++ index.js
@@ -24,29 +24,53 @@ async function run() {
         await client.connect();
         const taskCollection = client.db("etask").collection("task");
 
+        // API to get pending task
         app.get('/task', async (req, res) => {
             const query = { completed: false }
             const result = await taskCollection.find(query).toArray();
             res.send(result)
         })
 
-
+        // API to add pending task to the server
         app.post('/task', async (req, res) => {
             const query = req.body
             const result = await taskCollection.insertOne(query)
             res.send(result)
         })
 
+        // API to get completed task
         app.get('/completedtask', async (req, res) => {
             const query = { completed: true }
             const result = await taskCollection.find(query).toArray();
             res.send(result)
         })
 
+        app.get('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
 
+        app.put('/updatedTask/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateTask = req.body;
+            console.log(updateTask)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    taskName: updateTask.taskName,
+                    taskDetail: updateTask.taskDetail,
+                }
+            }
+
+            const result = await taskCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+
+        })
 
         app.patch('/task/:id', async (req, res) => {
-
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const updateDoc = {
@@ -54,7 +78,6 @@ async function run() {
                     completed: true,
                 }
             }
-
             const updateTask = await taskCollection.updateOne(filter, updateDoc);
             res.send(updateDoc);
 
